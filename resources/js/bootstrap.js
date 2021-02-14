6ce550e8c5dea1b6afcc87a523ec7a4759abef81
mizanur090148 <mizanur090148@gmail.com>
2021-02-15 04:27:39 +0600

    base url edit

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -10,7 +10,9 @@ window.axios = require('axios');
 
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 
-window.defaults.withCredentials = true;
+axios.defaults.withCredentials = true;
+//window.defaults.withCredentials = true;
+
 /**
  * Echo exposes an expressive API for subscribing to channels and listening
  * for events that are broadcast by Laravel. Echo and event broadcasting
